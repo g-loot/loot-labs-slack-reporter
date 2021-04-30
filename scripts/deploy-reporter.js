96690f09aca43a8573ba1cf21af7e672bd446905
Magnus Lidmyr <magnus.lidmyr@gmail.com>
2021-04-30 11:31:16 +0200

    hola

--- scripts/deploy-reporter.js
+++ scripts/deploy-reporter.js
@@ -2,7 +2,7 @@ const { exec } = require("child_process");
 const fetch = require("node-fetch");
 
 const SLACK_HOOK_URL = process.env.SLACK_HOOK_URL;
-console.log("last 4 chars of slack hook url: ", SLACK_HOOK_URL.slice(-10));
+console.log("last 10 chars of slack hook url: ", SLACK_HOOK_URL.slice(-10));
 async function cmd(command) {
   return new Promise((res, rej) => {
     exec(command, (error, stdout, stderr) => {
